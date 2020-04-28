54ad61cf04403d8055d8c3857c2734376cf4647e
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2020-04-28 13:58:08 +0545

    Add a endpoint

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
-
+#run scraping code regularly
 scraper.scrapeCovid()
+
 @app.route('/corona-data')
-def starting():
+def districtsApi():
     with open('CoronaNepal.json',mode="r", encoding='utf-8') as ff:
         coronaData = json.load(ff)
     return jsonify(coronaData)
+        
+@app.route('/')
+def home():
+    return jsonify({'msg':'working'})
